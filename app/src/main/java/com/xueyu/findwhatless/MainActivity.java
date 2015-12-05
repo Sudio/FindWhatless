bf24e1e177d408debbbe8001efa8717efae3e176
Sudio <68651655@qq.com>
2015-12-05 17:09:23 +0800

    Complete ZXing

--- app/src/main/java/com/xueyu/findwhatless/MainActivity.java
+++ app/src/main/java/com/xueyu/findwhatless/MainActivity.java
@@ -1,13 +1,36 @@
 package com.xueyu.findwhatless;
 
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.xys.libzxing.zxing.activity.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnCapture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCapture = (Button) findViewById(R.id.btn_capture);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openCameraIntent = new Intent(MainActivity.this,CaptureActivity.class);
+                startActivityForResult(openCameraIntent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("RESULT",data.getExtras().getString("result"));
     }
 }
